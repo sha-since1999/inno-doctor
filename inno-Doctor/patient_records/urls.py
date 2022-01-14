ae5e9fc454218f2c9159c4f9d4604758aa6f9298
Geetha <k.geetha@innovaccer.com>
2022-01-14 13:52:00 +0530

    backend for eprescription adding

--- inno-Doctor/patient_records/urls.py
+++ inno-Doctor/patient_records/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    PatientView, PatientList
+    EPrescription, PatientView, PatientList,
 )
 
 app_name = 'patient_records'
@@ -9,4 +9,6 @@ app_name = 'patient_records'
 urlpatterns = [
     path('patient_record_form/', PatientView, name='patient_record_form'),
     path('patient_record_list/', PatientList, name='patient_record_list'),
+    path('e_prescription/<int:aadhar_id>/', EPrescription,
+         name='e_prescription')
 ]
--- inno-Doctor/patient_records/views.py
+++ inno-Doctor/patient_records/views.py
@@ -11,6 +11,7 @@ def PatientView(request):
 def PatientList(request):
     aadhar_no = request.POST.get('aadharid')
     date_of_birth=request.POST.get('bdate')
+    print(date_of_birth)
     if (InternationalPatientSummary.objects.filter(aadhar_no=aadhar_no).exists()) and (InternationalPatientSummary.objects.filter(date_of_birth=date_of_birth).exists()):
         ips_id = InternationalPatientSummary.objects.get(
                 aadhar_no = aadhar_no
@@ -27,3 +28,31 @@ def PatientList(request):
     else:
         messages.error(request, 'Patient is not registered')
         return render(request, "patient_records/patient_record_form.html")
+
+
+def EPrescription(request, aadhar_no):
+    ips = InternationalPatientSummary.objects.filter(aadhar_no=aadhar_no)
+    if ips:
+        medication_statement = MedicationStatement.objects.create(ips=ips[0])
+        medication_items = []
+        for item in request.POST.get('items'):
+            med_item = MedicationItem(name=item["name"], form=item["form"],
+                                      medication_statement=medication_statement,
+                                      category=item["category"],
+                                      unit_of_prescription=item["unit_of_prescription"],
+                                      batch_id=item["batch_id"],
+                                      expiry=item["expiry"],
+                                      dose_amount=item["dose_amount"],
+                                      dose_duration=item["dose_duration"],
+                                      dose_unit=item["dose_unit"],
+                                      dose_frequency=item.get("dose_frequency"),
+                                      dose_interval=item.get("dose_interval"),
+                                      dose_specific_timing=item.get(
+                                              "dose_specific_timing"),
+                                      route=item["route"],
+                                      body_site=item["body_site"])
+
+            medication_items.append(med_item)
+        MedicationItem.objects.bulk_create(medication_items)
+    else:
+        raise ValueError
