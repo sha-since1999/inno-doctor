d1be21adf4312eea4f5585015c4693d2ea35565b
chandan <chku17ee@cmrit.ac.in>
2022-01-23 16:01:50 +0530

    Selenium tests for patients

--- inno-Doctor/patient_records/tests/seleniumtest/test_add_patient.py
+++ inno-Doctor/patient_records/tests/seleniumtest/test_add_patient.py
@@ -5,27 +5,67 @@ from selenium.webdriver.support.select import Select
 import time
 from selenium.webdriver import ActionChains
 import unittest
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 class PatientTest(unittest.TestCase):
-
-    def test_patient_add(self):
-	    # browser = self.selenium
-	    # url = self.live_server_url + '/login'
-	    # browser.get(url)
+    # @classmethod
+    def setUp(self):
+        global selenium
         selenium = webdriver.Chrome("chromedriver")
-        selenium.maximize_window() 
-        selenium.get('http://127.0.0.1:8000/patient_records/patient-create/')
-        AadharNumber=432143214321
-        Name_of_patiet="Samuel"
-        Dob="10/01/1998"
+        selenium.maximize_window()
+        selenium.get('http://127.0.0.1:8000/')
+       
+
+    def test_doctor_login(self):
+        loginbuton=selenium.find_element_by_css_selector("#navigation > ul > li:nth-child(3) > a").click()
+        # time.sleep(3)
+        email="thor@gmail.com"
+        password="thor@12345"
+        putemail=selenium.find_element_by_id("id_email")
+        putemail.send_keys(email)
+        putpassword=selenium.find_element_by_id("id_password")
+        putpassword.send_keys(password)
+    # click on sign in
+        sign=selenium.find_element_by_css_selector("body > div.card.card-body.blur.shadow-blur.mx-3.mx-md-4.mt-n6 > div > form > div.text-center > button").click()
+        time.sleep(5)
+        
+
+
+        # Add patient click
+        clickonaddpatient=selenium.find_element_by_xpath('//*[@id="navigation"]/ul/li[2]/a/p').click()
+        actual_url="http://127.0.0.1:8000/patient_records/patient-detail/923412347234"
+        AadharNumber="923412347234"
+        Name_of_patiet="Sammi"
+        Dob="10/01/1994"
         AadharId=selenium.find_element_by_name("aadhaarId").send_keys(AadharNumber)
         Name=selenium.find_element_by_name("name").send_keys(Name_of_patiet)
         dateofbirth=selenium.find_element_by_name("date_of_birth").send_keys(Dob)
         select = Select(selenium.find_element_by_name("gender"))
         select.select_by_visible_text("Male")
-        AddPatient=selenium.find_element_by_css_selector("body > div.card.card-body.blur.shadow-blur.mx-3.mx-md-4.mt-n6 > div > form > div.text-center > button").click()
-        time.sleep(5)
+
+        button = selenium.find_element_by_name("addpatient")
+        selenium.execute_script("arguments[0].click();", button)
+        time.sleep(2)
+
+
+
+
+
+
+
+          # Not in use.
+
+#         expected_url=selenium.current_url
+#         self.assertEqual(actual_url,expected_url)
+#         # element = selenium.find_elements_by_class_name('div[class*="btn bg-gradient-info w-40 my-4 mb-2 mx-1"]')
+#         # selenium.execute_script("arguments[0].click();", element)
+#         print("my page title=",selenium.title)
+#         time.sleep(2)
+#         # self.assertEqual(len(AadharNumber),self.MaxDigit)
+#         # assert 'Lebron James' in selenium.page_source
+    
 
 
 
@@ -34,13 +74,47 @@ class PatientTest(unittest.TestCase):
 
 
 
-	    
 
 
 
 
 
 
+
+
+    # def test_patient_add(self):
+	    # browser = self.selenium
+	    # url = self.live_server_url + '/login'
+	    # browser.get(url)
+        # actual_url="http://127.0.0.1:8000/patient_records/patient-detail/923412347234"
+        # AadharNumber="923412347234"
+        # Name_of_patiet="Sam"
+        # Dob="10/01/1998"
+        # AadharId=selenium.find_element_by_name("aadhaarId").send_keys(AadharNumber)
+        # Name=selenium.find_element_by_name("name").send_keys(Name_of_patiet)
+        # dateofbirth=selenium.find_element_by_name("date_of_birth").send_keys(Dob)
+        # select = Select(selenium.find_element_by_name("gender"))
+        # select.select_by_visible_text("Male")
+        # # AddPatient=selenium.find_element_by_name("addpatient").click()
+         
+        # button = selenium.find_element_by_name("addpatient")
+        # selenium.execute_script("arguments[0].click();", button)
+        # time.sleep(2)
+        # expected_url=selenium.current_url
+        # self.assertEqual(actual_url,expected_url)
+        # # element = selenium.find_elements_by_class_name('div[class*="btn bg-gradient-info w-40 my-4 mb-2 mx-1"]')
+        # # selenium.execute_script("arguments[0].click();", element)
+        # print("my page title=",selenium.title)
+        # time.sleep(2)
+        # # self.assertEqual(len(AadharNumber),self.MaxDigit)
+        # # assert 'Lebron James' in selenium.page_source
+    
+
+
+    # @classmethod
+    def tearDown(self):
+        time.sleep(10)
+        selenium.close()
 
 
 
---
+++ inno-Doctor/patient_records/tests/seleniumtest/test_view_med_stat.py
@@ -0,0 +1,72 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+from selenium.webdriver.support.select import Select
+import time
+
+from selenium.webdriver import ActionChains
+import unittest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+class PatientTest(unittest.TestCase):
+    # @classmethod
+    def setUp(self):
+        global selenium
+        selenium = webdriver.Chrome("chromedriver")
+        selenium.maximize_window()
+        selenium.get('http://127.0.0.1:8000/')
+       
+
+    def test_check_patient(self):
+        loginbuton=selenium.find_element_by_css_selector("#navigation > ul > li:nth-child(3) > a").click()
+        # time.sleep(3)
+        email="thor@gmail.com"
+        password="thor@12345"
+        putemail=selenium.find_element_by_id("id_email")
+        putemail.send_keys(email)
+        putpassword=selenium.find_element_by_id("id_password")
+        putpassword.send_keys(password)
+    # click on sign in
+        sign=selenium.find_element_by_css_selector("body > div.card.card-body.blur.shadow-blur.mx-3.mx-md-4.mt-n6 > div > form > div.text-center > button").click()
+        time.sleep(5)
+
+        # click on check patient button.patient_check portion.
+        button=selenium.find_element_by_name('ck_patient')
+        selenium.execute_script("arguments[0].click();", button)
+        
+        #put id in box
+        Aadharkey="923412347234"
+       
+        enter=selenium.find_element_by_name("aadhaarId")
+        enter.send_keys('923412347234')
+        enter.send_keys(Keys.ENTER)
+
+        actual_url="http://127.0.0.1:8000/patient_records/patient-eprescription/923412347234"
+
+# To Check Medication Statements
+        Submitbutton=selenium.find_element_by_css_selector("body > div.card.card-body.blur.shadow-blur.mx-3.mx-md-4.mt-n6 > div > section:nth-child(4) > div > div > div > div > ul > li:nth-child(1) > a")
+        selenium.execute_script("arguments[0].click();", Submitbutton)
+        expected_url=selenium.current_url
+        self.assertEqual(actual_url,expected_url)
+
+        
+
+
+
+
+
+
+
+   # @classmethod
+    def tearDown(self):
+        time.sleep(10)
+        selenium.close()
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
