fcc4b688dee709296b4668143cbfd77fcd505e49
Rohit sahu <53229346+sha-since1999@users.noreply.github.com>
2022-01-23 14:48:40 +0530

    change production settings

--- inno-Doctor/app/settings.py
+++ inno-Doctor/app/settings.py
@@ -1,8 +1,8 @@
 import os
 
 IS_PRODUCTION = os.environ.get('IS_PRODUCTION', ' ') != 'False'
-IS_PRODUCTION = False
-print('IP', IS_PRODUCTION)
+# IS_PRODUCTION = False
+# print('IP', IS_PRODUCTION)
 
 if IS_PRODUCTION:
     from .conf.production.settings import *
