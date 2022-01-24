dbbaec4b6f957743f8e1325ef8fcd8e2c9152114
Shikhar <ptksuraj99@gmail.com>
2022-01-24 11:25:01 +0530

    fix: deleted dev settings

--- inno-Doctor/app/settings.py
+++ inno-Doctor/app/settings.py
@@ -5,9 +5,11 @@ IS_PRODUCTION = os.environ.get('IS_PRODUCTION', ' ') != 'False'
 # IS_PRODUCTION = False
 # print('IP', IS_PRODUCTION)
 
-if IS_PRODUCTION:
-    from .conf.production.settings import *
-    print("Production Mode")
-else:
-    from .conf.development.settings import *
-    print("Development Mode")
+# if IS_PRODUCTION:
+#     from .conf.production.settings import *
+#     print("Production Mode")
+# else:
+#     from .conf.development.settings import *
+#     print("Development Mode")
+
+from .conf.production.settings import *
