f3be62b13e5c3ec26b3fba2e32100907ed94ec54
sha-since1999 <rohits8853@gmail.com>
2022-01-24 13:14:25 +0530

    update pdf genrate feature

--- inno-Doctor/patient_records/views.py
+++ inno-Doctor/patient_records/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404, render_to_response
-
+from .pdf_utils import *
 from django.shortcuts import redirect
 
 from django.shortcuts import render
