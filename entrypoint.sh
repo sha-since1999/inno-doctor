37f1ae05605337b02eb110f21b2f71aace2dc4d2
Shikhar <ptksuraj99@gmail.com>
2022-01-24 12:10:53 +0530

    CI tests fix #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-python manage.py runserver 0.0.0.0:8000 --no-input
+python manage.py runserver 0.0.0.0:8000
 echo "Hii"
