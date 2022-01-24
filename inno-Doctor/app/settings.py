4352191a223a6f8da34059c80caa928a75eb929a
Shikhar <ptksuraj99@gmail.com>
2022-01-24 13:22:46 +0530

    fix: ci trial #2

--- inno-Doctor/app/settings.py
+++ inno-Doctor/app/settings.py
@@ -5,11 +5,9 @@ IS_PRODUCTION = os.environ.get('IS_PRODUCTION', ' ') != 'False'
 # IS_PRODUCTION = False
 # print('IP', IS_PRODUCTION)
 
-# if IS_PRODUCTION:
-#     from .conf.production.settings import *
-#     print("Production Mode")
-# else:
-#     from .conf.development.settings import *
-#     print("Development Mode")
-
-from .conf.production.settings import *
+if IS_PRODUCTION:
+    from .conf.production.settings import *
+    print("Production Mode")
+else:
+    from .conf.development.settings import *
+    print("Development Mode")
